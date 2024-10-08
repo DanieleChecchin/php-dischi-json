37836791f386b6e33de08f404bc0b66d17fb68b7
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-08 15:50:09 +0200

    chiamata API

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            dischi: [],
+            disksList: [],
         }
-    }
+    },
+    methods: {
+        // Chiamata API
+        getDisks() {
+            axios.get('http://localhost/php-dischi-json/api/')
+                .then((response) => {
+                    // handle success
+                    console.log(response.data);
+                    this.disksList = response.data;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+        }
+    },
+    created() {
+        this.getDisks();
+    },
+
+
+
 }).mount('#app')
